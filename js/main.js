4b87deb015fcb180a5fe95848f41448a8b4e729d
kiibabe <51504168+kiibabe@users.noreply.github.com>
2021-09-19 10:16:57 +0900

    Completed week01 index

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
+window.addEventListener('load', list)
+
 const links = [
     {
       label: "Week1 notes",
       url: "week1/index.html"
-    },
-    {
-        label: "Week2 notes",
-        url: "week2/index.html"
-      }
-  ]
-  
-for (const e of links) {
-    document.getElementById("test").innerHTML += '<li><a href="' + e.url + '">' + e.label + '</a></li>';
+    }
+]
+
+function list() {
+    for (const e of links) {
+        document.getElementById("list").innerHTML += '<li><a href="' + e.url + '">' + e.label + '</a></li>';
+    }
 }
