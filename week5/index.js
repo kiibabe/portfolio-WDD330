deded7ffef933fe97622992f545db54f4166f72f
kiibabe <51504168+kiibabe@users.noreply.github.com>
2021-10-13 13:49:17 +0900

    delete team04

--- week5/index.js
+++
@@ -1,6 +0,0 @@
-import HikesController from './hikesController.js';
-const myHikesController = new HikesController('hikes');
-// show the array on load
-window.addEventListener('load', function() {
-    myHikesController.showHikeList();
-});
